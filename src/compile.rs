626d238a858e0542a9a6b61b396743d827b8f303
TheArchailect <58282672+TheArchailect@users.noreply.github.com>
2026-04-05 18:48:39 +0800

    Remove dependency-only metadata from SPIR-V builder

--- src/compile.rs
+++ src/compile.rs
@@ -191,7 +191,6 @@ pub(crate) fn compile_shaders(
 
     builder
         .spirv_metadata(SpirvMetadata::NameVariables)
-        .spirv_metadata(MetadataPrintout::DependencyOnly)
         .build()?;
 
     Ok(())
